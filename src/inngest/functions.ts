9e81ec1094e4d0f717ee21fb2fa17c1fa3a0ecbc
Tushar Chauhan <tusharchauhan0912@gmail.com>
2025-12-29 10:43:42 +0530

    feat: add E2B code interpreter and integrate with Inngest functions
    - Added @e2b/code-interpreter dependency to package.json and package-lock.json.
    - Created compile_page.sh script to ensure Next.js app is running and compiled.
    - Added Dockerfile for setting up the Next.js environment with necessary dependencies.
    - Configured e2b.toml for sandbox template settings.
    - Enhanced Inngest function to create and connect to a sandbox, returning the sandbox URL.
    - Implemented utility function to connect to the sandbox.

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,25 +1,44 @@
 import { inngest } from "./client";
-
 import { Agent, openai, createAgent } from "@inngest/agent-kit";
+import { Sandbox } from '@e2b/code-interpreter';
+
+import { getSandbox } from "./utils";
+import { get } from "http";
 
 // Inngest Function
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
+
+    // Create a sandbox
+    // get sandbox id
+    const sandboxId = await step.run("get-sandbox-id", async () =>{
+      // Create a new sandbox
+      const sandbox = await Sandbox.create("easzz-dev-nextjs-test");
+      return sandbox.sandboxId;
+    })
+
+
     // Agent Creation
     const codeAgent = createAgent({
       name: "codeAgent",
       system: "You are an expert NextJs Developer. You write readable and maintainable code. You write simple NextJs & React code snippets.",
       model: openai({ model: "gpt-4o" }),
     });
-
     const { output } = await codeAgent.run(
       ` Write the following snippets : ${event.data.value}`
     );
-
     console.log(`code: ${output}`);
 
-    return { code: output };
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const sandboxHost = sandbox.getHost(3000);
+      return `http://${sandboxHost}`;
+    });
+
+    console.log(`Sandbox URL: ${sandboxUrl}`);
+
+    return { code: output , sandboxUrl };
   }
 );
