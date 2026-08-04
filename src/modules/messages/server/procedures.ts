00f91797e8cb4a19273016d58ae30d3bd93940ab
Tushar Chauhan <tusharchauhan0912@gmail.com>
2026-08-04 21:03:13 +0530

    feat: update package dependencies, enhance project and message models, and add project view and messages container components

--- src/modules/messages/server/procedures.ts
+++ src/modules/messages/server/procedures.ts
@@ -1,42 +1,87 @@
-import { baseProcedure, createTRPCRouter } from "@/trpc/init";
-import prisma from "@/lib/db";
 import { z } from "zod";
+import { TRPCError } from "@trpc/server";
+
+import { prisma } from "@/lib/db";
 import { inngest } from "@/inngest/client";
+import { protectedProcedure, createTRPCRouter } from "@/trpc/init";
+import { consumeCredits } from "@/lib/usage";
 
 export const messagesRouter = createTRPCRouter({
-    getMany : baseProcedure
-    .query(async ()=>{
-        const messages = await prisma.message.findMany({
-            orderBy : {
-                createdAt : "desc",
-            },
-            include: {
-                fragment : true,
-            }
-        });
-        return messages;
-    }),
-
-    create: baseProcedure
-    .input(
-        z.object({
-            Value: z.string().min(1,{message : "Message cannot be empty"}),
-        }),
+  getMany: protectedProcedure
+  .input(
+      z.object({
+        projectId: z.string().min(1, { message: "Project ID is required" }),
+      }),
     )
-    .mutation(async ({input})=>{
-        const createdMessage = await prisma.message.create({
-            data : {
-                content : input.Value,
-                role : "USER",
-                type : "RESULT",
-            }
-        });
+    .query(async ({ input, ctx }) => {
+      const messages = await prisma.message.findMany({
+        where: {
+          projectId: input.projectId,
+          project: {
+            userId: ctx.auth.userId,
+          },
+        },
+        include: {
+          fragment: true,
+        },
+        orderBy: {
+          updatedAt: "asc",
+        },
+      });
 
-        await inngest.send({
-            name : "test/hello.world",
-            data : {value : input.Value}
-        })
+      return messages;
+    }),
+  create: protectedProcedure
+    .input(
+      z.object({
+        value: z.string()
+          .min(1, { message: "Value is required" })
+          .max(10000, { message: "Value is too long" }),
+        projectId: z.string().min(1, { message: "Project ID is required" }),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      const existingProject = await prisma.project.findUnique({
+        where: {
+          id: input.projectId,
+          userId: ctx.auth.userId,
+        },
+      });
 
-        return createdMessage;
-    })
+      if (!existingProject) {
+        throw new TRPCError({ code: "NOT_FOUND", message: "Project not found" });
+      }
+
+      try {
+        await consumeCredits();
+      } catch (error) {
+        if (error instanceof Error) {
+          throw new TRPCError({ code: "BAD_REQUEST", message: "Something went wrong" });
+        } else {
+          throw new TRPCError({
+            code: "TOO_MANY_REQUESTS",
+            message: "You have run out of credits"
+          });
+        }
+      }
+
+      const createdMessage = await prisma.message.create({
+        data: {
+          projectId: existingProject.id,
+          content: input.value,
+          role: "USER",
+          type: "RESULT",
+        },
+      });
+
+      await inngest.send({
+        name: "code-agent/run",
+        data: {
+          value: input.value,
+          projectId: input.projectId,
+        },
+      });
+
+      return createdMessage;
+    }),
 });
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -2,15 +2,16 @@ import { z } from "zod";
 import { baseProcedure, createTRPCRouter } from "../init";
 import { inngest } from "@/inngest/client";
 import { messagesRouter } from "@/modules/messages/server/procedures";
+import { projectsRouter } from '@/modules/projects/server/procedures';
 
 export const appRouter = createTRPCRouter({
   // Example: Inngest Event Invocation using trpc
 
   messages: messagesRouter,
+  projects: projectsRouter,
 
   // fragment : fragmentRouter,
-
 });
-  
+
 // export type definition of API 
 export type AppRouter = typeof appRouter;
